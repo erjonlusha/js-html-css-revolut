236359b036c876d260c43c02d16dc9682ca860f1
Erjon <erjonlusha@gmail.com>
2020-11-30 16:34:54 +0100

    Creata semi-struttura della nav

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+/**********************************     Descrizione:
+Usado jQuery riproducete l'effetto dei menu a tendina del sito revolut (https://www.revolut.com/)
+usate lo screenshot di seguito per riprodurre anche il layout della navbar.
+Attenzione:
+Dovete riprodurre solo la navbar ed i menu a tendina, 
+non tutto il sito. 
+Fate attenzione che in questo caso non abbiamo un solo menu a tendina ma piú di uno. 
+Il menu deve diventare visibile quando facciamo hover  o click ( a vs discrezione ) su uno dei link principali nella navbar. 
+Ogni link della navbar ha il suo menu nascosto che deve apparire.
+*******************************/
+
